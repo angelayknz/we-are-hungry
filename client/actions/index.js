a73855530902cfe6183d95ef2fc8e21ec39af062
dhdaveidhd <46812457+dhdaveidhd@users.noreply.github.com>
2022-06-30 18:07:58 +1200

    complete recipe fetching and displaying 4 items

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,24 +1,44 @@
 // THIS IS WHERE THE ACTION CREATORS WILL GO
 import request from 'superagent'
 
+// action types - SNAKE_CASE
 export const RECEIVE_RECIPES = 'RECEIVE_RECIPES'
+export const REQUEST_RECIPES = 'REQUEST_RECIPES'
+export const RECEIVE_RECIPES_ERROR = 'RECEIVE_RECIPES_ERROR'
 
+// action creators - functions - camelCase
 export function recieveRecipes(recipes) {
   return {
     type: RECEIVE_RECIPES,
-    payload: recipes.meals,
+    payload: recipes,
+  }
+}
+
+export function requestRecipes() {
+  return {
+    type: REQUEST_RECIPES,
+  }
+}
+
+export function requestRecipesError(err) {
+  return {
+    type: RECEIVE_RECIPES_ERROR,
+    payload: err,
   }
 }
 
 export function fetchRecipes() {
   return (dispatch) => {
+    dispatch(requestRecipes())
     return request
-      .get('www.themealdb.com/api/json/v1/1/search.php?f=a')
+      .get('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+      .set('Accept', 'application/json')
       .then((res) => {
-        dispatch(recieveRecipes(res.body))
+        dispatch(recieveRecipes(res.body.meals))
         return null
       })
       .catch((err) => {
+        dispatch(requestRecipesError(err))
         console.log(err)
       })
   }
--- client/reducers/showRecipe.js
+++ client/reducers/showRecipe.js
@@ -1,11 +1,19 @@
-import { RECEIVE_RECIPES } from '../actions'
+import {
+  RECEIVE_RECIPES,
+  REQUEST_RECIPES,
+  RECEIVE_RECIPES_ERROR,
+} from '../actions'
 
-function recipes(state = [], action) {
+function recipes(state = { data: [], loading: false, error: null }, action) {
   const { type, payload } = action
 
   switch (type) {
     case RECEIVE_RECIPES:
-      return payload
+      return { ...state, data: payload, loading: false, error: null }
+    case REQUEST_RECIPES:
+      return { ...state, loading: true, error: null }
+    case RECEIVE_RECIPES_ERROR:
+      return { ...state, loading: false, error: payload }
     default:
       return state
   }
